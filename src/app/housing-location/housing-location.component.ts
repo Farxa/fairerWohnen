e3935e1e4a51e8c229704d7556bbf9b0a28ec185
farxa <r.abualhassan@gmail.com>
2024-04-18 19:19:59 +0200

    add @Input() decorator and use non-null assertion to notify compiler that value of new property won't be null or undefined

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
@@ -14,5 +14,5 @@ import { CommonModule } from '@angular/common';
 })
 
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
